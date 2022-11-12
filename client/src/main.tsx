120d54140cfaabe597c44b9ec88baba4cd9adbb0
Johnny Chai <hello@johnnychai.com>
2022-11-12 15:50:27 +0800

    feat: add images & styling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
-import "./assets/style/index.css";
+import "./assets/styles/index.css";
 
 // Entry point for the client-side React app.
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,15 +2,29 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        commissioner: ["commissioner", "sans-serif"],
+      },
+    },
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+    },
     colors: {
       white: "#FFFFFF",
-      gray: "#F5F6F8",
-      darkGray: "#e1e1e1",
-      black: "#3F3F3F",
-      red: "#fd3636",
-      blue: "#366ffd",
-      green: "#36fd82",
+      "purple-100": "#EAECFF",
+      "purple-500": "#BCC3FF",
+      "purple-700": "#858AB5",
+      "purple-800": "#676B8C",
+      "purple-900": "#4F526B",
+      "grey-50": "#EBEBEB",
+      "grey-100": "#C0C0C0",
+      "grey-300": "#767676",
+      "grey-500": "#333333",
+      "grey-900": "#151515",
     },
   },
   plugins: [],
