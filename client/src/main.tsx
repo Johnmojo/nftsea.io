3330f90231d84ac01b7c1e5a2ef6137225794916
Johnny Chai <hello@johnnychai.com>
2022-11-12 15:00:57 +0800

    fix: remove folders

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Index from "./pages";
+import Home from "./pages/Home";
 import "./assets/style/index.css";
 
+// Entry point for the client-side React app.
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Index />
+    <Home />
   </React.StrictMode>
 );
