868d0827dd2f73599b4148a7f3724be70fcced1a
Johnny Chai <hello@johnnychai.com>
2022-11-25 18:10:23 +0800

    fix: slider

--- client/src/features/rankings/sections/RankingSection.tsx
+++ client/src/features/rankings/sections/RankingSection.tsx
@@ -25,7 +25,7 @@ const RankingSection = () => {
   return (
     <section>
       <div className="w-full px-4 sm:px-8 xl:px-24 md:mt-[104px] md:mb-[160px] sm:my-[128px] my-[64px]">
-        <div className="flex flex-col md:flex-row mx-auto justify-between gap-12 relative bg-purple-100 rounded-3xl max-w-screen-xl md:px-12 md:py-12 sm:p-8 p-6">
+        <div className="flex flex-col md:flex-row mx-auto justify-between gap-12 relative bg-purple-100 rounded-3xl max-w-screen-xl md:px-12 md:py-0 sm:p-8 p-6">
           <div className="md:space-y-14 sm:space-y-12 space-y-8 flex flex-col items-center md:items-start justify-center w-full md:w-1/2">
             <div className="w-full md:w-3/4 md:text-left text-center">
               <p className="font-medium text-purple-900 md:text-base text-xs pb-1.5">
@@ -47,7 +47,7 @@ const RankingSection = () => {
           </div>
           <div className="flex justify-end w-full md:w-1/2">
             <div className="md:overflow-auto overflow-x-scroll h-auto md:scrollbarVertical sm:scrollbarHorizontalTabletWhite scrollbarHorizontalMobileWhite">
-              <div className="flex md:flex-col flex-row md:space-y-12 md:space-x-0 sm:space-x-8 space-x-5 select-none md:h-[600px] h-fit pr-[20%] sm:pb-16 pb-12">
+              <div className="flex md:flex-col flex-row md:space-y-12 md:space-x-0 sm:space-x-8 space-x-5 select-none md:h-[600px] h-fit pr-[20%] sm:pb-16 mt-12 pb-12">
                 {data.map((ranking: rankingProps, index: number) => (
                   <Card
                     key={index}
